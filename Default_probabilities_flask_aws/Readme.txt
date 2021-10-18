a15217efde56f84791db9a51f53101a0680ea70a
Victor Kovatsenko <victorkovatsenko@gmail.com>
2021-10-18 19:31:14 +0200

    Default proba project added

---
+++ Default_probabilities_flask_aws/Readme.txt
@@ -0,0 +1,3 @@
+"Kovatsenko_Victor_test_model_creation" - jupyther notebook with step by step model creation
+"App" folder - Flask microservices app 
+"Dockerrun.aws.json" - json file to deploy container on AWS Beanstalk
---
+++ Default_probabilities_flask_aws/app/features.py
@@ -0,0 +1,8 @@
+FEATURES = ['account_days_in_dc_12_24m', 'account_days_in_rem_12_24m', 'age', 'avg_payment_span_0_12m',
+            'max_paid_inv_0_12m', 'num_active_div_by_paid_inv_0_12m', 'num_arch_dc_12_24m', 'num_arch_ok_0_12m',
+            'num_unpaid_bills', 'recovery_debt', 'account_status', 'account_worst_status_0_3m',
+            'account_worst_status_12_24m', 'account_worst_status_3_6m', 'account_worst_status_6_12m',
+            'merchant_category', 'merchant_group', 'name_in_email', 'status_last_archived_0_24m',
+            'status_2nd_last_archived_0_24m', 'status_3rd_last_archived_0_24m', 'status_max_archived_0_6_months',
+            'status_max_archived_0_12_months', 'status_max_archived_0_24_months', 'worst_status_active_inv',
+            'has_paid']
